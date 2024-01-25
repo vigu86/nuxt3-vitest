b9d51c1560997080f93f807d33ee551eef9c746c
Marcin Dabkus <marcindabkus@gmail.com>
2024-01-25 22:33:44 +0100

    Setup Tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
